2f2cc61097d7f4516654d6f6f2f1d8121aedfb72
scutwinter <scutwinter@vip.qq.com>
2019-06-13 01:12:57 +0800

    ngSwitch 练习

--- lesson4/src/app/app.module.ts
+++ lesson4/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+  import { AppRoutingModule } from './app-routing.module';
+  import { AppComponent } from './app.component';
 
-@NgModule({
+  import { NgSwitchSampleApp } from './ng_switch/ng_switch';
+  import { NgSwitchSampleAppModule } from './ng_switch/ng_switch';
+
+  @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgSwitchSampleAppModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NgSwitchSampleApp]
 })
 export class AppModule { }
+

6aeb562ea87d293bf941015072e948205d56ce6e
scutwinter <scutwinter@vip.qq.com>
2019-09-07 22:33:53 +0800

    SpringStudy 2019-09-07

--- highlig_spring4/src/main/java/com/winter/highlight_spring4/ch2/profile/ProfileConfig.java
+++ highlig_spring4/src/main/java/com/winter/highlight_spring4/ch2/profile/ProfileConfig.java
@@ -17,4 +17,5 @@ public class ProfileConfig {
     public DemoBean prodDemoBean(){
         return new DemoBean("from production profile");
     }
+
 }
---
+++ highlig_spring4/src/main/java/com/winter/highlight_spring4/ch3/taskscheduler/TaskSchedulerConfig.java
@@ -0,0 +1,11 @@
+package com.winter.highlight_spring4.ch3.taskscheduler;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@ComponentScan("com.winter.highlight_spring4.ch3.taskscheduler")
+@EnableScheduling //1通过@EnableScheduling注解开启对计划任务的支持
+public class TaskSchedulerConfig {
+}

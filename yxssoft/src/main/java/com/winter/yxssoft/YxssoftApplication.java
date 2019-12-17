72ab4a6f168528b691ee7ce5c83edb23268d37d1
scutwinter <scutwinter@vip.qq.com>
2019-12-17 14:09:57 +0800

    20191217 Study

---
+++ yxssoft/src/main/java/com/winter/yxssoft/YxssoftApplication.java
@@ -0,0 +1,14 @@
+package com.winter.yxssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxssoftApplication {
+
+    public static void main(String[] args) {
+        System.out.println("启动 spring boot……");
+        SpringApplication.run(YxssoftApplication.class, args);
+    }
+
+}

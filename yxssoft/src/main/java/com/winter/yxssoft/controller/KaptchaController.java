9be5f2a01e26c84aff31afb78e7ed2d1646d0d47
scutwinter <scutwinter@vip.qq.com>
2019-12-25 02:01:21 +0800

    20191224 博客系统 验证码功能

--- yxssoft/src/main/java/com/winter/yxssoft/controller/KaptchaController.java
+++ yxssoft/src/main/java/com/winter/yxssoft/controller/KaptchaController.java
@@ -3,12 +3,16 @@ package com.winter.yxssoft.controller;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 
@@ -39,5 +43,18 @@ public class KaptchaController {
         responseOutputStream.write(captchaOutputStream);
         responseOutputStream.flush();
         responseOutputStream.close();
+    }
+
+    @GetMapping("/verify")
+    @ResponseBody
+    public String verify(@RequestParam("code") String code, HttpSession session){
+        if(StringUtils.isEmpty(code)){
+            return "验证码不能为空";
+        }
+        String kaptchaCode = session.getAttribute("verifyCode") + "";
+        if (StringUtils.isEmpty(kaptchaCode) || !code.equals(kaptchaCode)){
+            return "验证码错误";
+        }
+        return "验证成功";
     }
 }

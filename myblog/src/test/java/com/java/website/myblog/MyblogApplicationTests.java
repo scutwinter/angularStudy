65ad3b0465233a91f22f3f2dd979ea6e27a64513
scutwinter <scutwinter@vip.qq.com>
2019-12-25 01:55:50 +0800

    20191225 MyBlog

---
+++ myblog/src/test/java/com/java/website/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.website.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

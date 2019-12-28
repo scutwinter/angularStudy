049b30bbb03cd75523fd45d06057c823704929de
scutwinter <scutwinter@vip.qq.com>
2019-12-28 14:34:48 +0800

    20191225 MyBlog

--- myblog/src/main/java/com/java/website/myblog/MyblogApplication.java
+++ myblog/src/main/java/com/java/website/myblog/MyblogApplication.java
@@ -1,9 +1,11 @@
 package com.java.website.myblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.java.website")
 public class MyblogApplication {
 
     public static void main(String[] args) {

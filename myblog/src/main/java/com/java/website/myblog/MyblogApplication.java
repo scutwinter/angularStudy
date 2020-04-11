740e9aaa1a6da44fbc72ab263e10315a88213a41
scutwinter <scutwinter@vip.qq.com>
2020-04-12 01:34:29 +0800

    20200412 项目完成

--- myblog/src/main/java/com/java/website/myblog/MyblogApplication.java
+++ myblog/src/main/java/com/java/website/myblog/MyblogApplication.java
@@ -3,13 +3,19 @@ package com.java.website.myblog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.java.website.myblog.dao")
-public class MyblogApplication {
+public class MyblogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MyblogApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyblogApplication.class);
+    }
 }
--- myblog/src/main/java/com/java/website/myblog/entity/BlogComment.java
+++ myblog/src/main/java/com/java/website/myblog/entity/BlogComment.java
@@ -1,7 +1,5 @@
 package com.java.website.myblog.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.util.Date;
 
 public class BlogComment {
@@ -17,14 +15,12 @@ public class BlogComment {
 
     private String commentBody;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date commentCreateTime;
 
     private String commentatorIp;
 
     private String replyBody;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date replyCreateTime;
 
     private Byte commentStatus;

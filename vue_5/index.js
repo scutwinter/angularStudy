413fee6393c5d585017281a0f4a57c94d1dba54e
scutwinter <scutwinter@vip.qq.com>
2020-05-16 23:03:01 +0800

    20200515 Vue.js实战第五章练习_将商品列表改成二维数组，实现商品分类，同类商品聚合到一起。

--- vue_5/index.js
+++ vue_5/index.js
@@ -5,23 +5,53 @@ var app = new Vue({
     data: {
         list: [
             {
-                id:1,
-                name:'iphone 7',
-                price:6188,
-                count:1
+                category:'电子产品',
+                product:[
+                    {
+                        id:1,
+                        name:'iphone 7',
+                        price:6188,
+                        count:1
+                    },
+                    {
+                        id:2,
+                        name:'iPad Pro',
+                        price:5888,
+                        count:1
+                    },
+                    {
+                        id:3,
+                        name:'MacBook Pro',
+                        price:21488,
+                        count:1
+                    }
+                ]
             },
             {
-                id:2,
-                name:'iPad Pro',
-                price:5888,
-                count:1
-            },
-            {
-                id:1,
-                name:'MacBook Pro',
-                price:21488,
-                count:1
+                category:'生活用品',
+                product:[
+                    {
+                        id:4,
+                        name:'小米电视机',
+                        price:4588,
+                        count:1
+                    },
+                    {
+                        id:5,
+                        name:'格力空调',
+                        price:6988,
+                        count:1
+                    },
+                    {
+                        id:6,
+                        name:'西门子冰箱',
+                        price:9880,
+                        count:1
+                    }
+                ]
             }
+
+
         ],
         listSelect:[]
 
@@ -49,31 +79,32 @@ var app = new Vue({
     },
     methods:{
 
-        handleReduce: function (index) {
-            if(this.list[index].count===1) return;
-            this.list[index].count--;
+        handleReduce: function (tableindex,index) {
+            if(this.list[tableindex].product[index].count===1) return;
+            this.list[tableindex].product[index].count--;
         },
-        handleAdd: function (index) {
-            this.list[index].count++;
+        handleAdd: function (tableindex,index) {
+            this.list[tableindex].product[index].count++;
         },
-        handleRemove: function (index) {
-            this.list.splice(index,1)
+        handleRemove: function (tableindex,index) {
+            this.list[tableindex].product.splice(index,1)
         },
-        handleSelect:function (index,e) {
-            function getRemoveIndex(index) {
+        handleSelect:function (tableindex,index,e) {
+
+            function getRemoveIndex(tableindex,index) {
                 for(var i=0;i<app.listSelect.length;i++)
                 {
-                    if(app.listSelect[i].id==app.list[index].id)
+                    if(app.listSelect[i].id==app.list[tableindex].product[index].id)
                         return i;
                 }
                 return 0;
 
             }
             if(e.target.checked==true)
-                this.listSelect.push(this.list[index]);
+                this.listSelect.push(this.list[tableindex].product[index]);
             else
             {
-                this.listSelect.splice(getRemoveIndex(index));
+                this.listSelect.splice(getRemoveIndex(tableindex,index));
             }
 
         }
